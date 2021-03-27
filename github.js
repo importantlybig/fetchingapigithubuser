b2e058982214153f325783377470ebfd8d2d2d57
trongdai <trongdai27122000@gmail.com>
2021-03-27 20:28:23 +0700

    finish fetching 10 latest repos of github user

--- github.js
+++ github.js
@@ -2,6 +2,8 @@ class Github {
     constructor() {
       this.client_id = '66c3fecc7c41bc1a4644';
       this.client_secret = 'f507bf42ce2917b6b81577692521f5099e0c8e61';
+      this.repos_count = 10;
+      this.repos_sort = 'created: asc';
     }
   
     async getUser(user) {
@@ -9,10 +11,16 @@ class Github {
         `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
       );
 
+      const repoResponse = await fetch(
+        `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+      );
+
       const profile = await profileResponse.json();
-    
+      const repos = await repoResponse.json();  
+
       return {
-        profile
+        profile,
+        repos
       };
     }
   }
